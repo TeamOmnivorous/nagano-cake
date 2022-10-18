ba2fb8e09236b41d7c36879e38d706996ddd8df7
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-10-18 05:27:05 +0000

    [Add] カラム名追加

--- db/migrate/20221017042525_create_cart_items.rb
+++ db/migrate/20221017042525_create_cart_items.rb
@@ -2,6 +2,10 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
 
+      t.references :customer, foreign_key: true, null: false
+      t.references :items, foreign_key: true, null: false
+      t.integer :amount, null: false
+
       t.timestamps
     end
   end

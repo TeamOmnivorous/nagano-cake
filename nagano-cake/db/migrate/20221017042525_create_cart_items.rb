18401448e73527eca0a72f6f785308f99fa8cb44
AsheDuran <bady.0222.monk@gmail.com>
2022-10-18 07:41:34 +0000

    todaやり直し

---
+++ nagano-cake/db/migrate/20221017042525_create_cart_items.rb
@@ -0,0 +1,12 @@
+class CreateCartItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cart_items do |t|
+
+      t.references :customer, foreign_key: true, null: false
+      t.references :items, foreign_key: true, null: false
+      t.integer :amount, null: false
+
+      t.timestamps null: false
+    end
+  end
+end

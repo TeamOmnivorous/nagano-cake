ac7abbd2994e86874604971222587c91516c4a55
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-10-18 05:22:06 +0000

    ADD model column [orders,order_items,deliveries]

--- db/migrate/20221017060959_create_orders.rb
+++ db/migrate/20221017060959_create_orders.rb
@@ -1,6 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.references :customer, foreign_key: true, null: false
+      t.integer :pay_type, null: false
+      t.integer :total_payment, null: false
+      t.integer :order_status, null: false
+      t.string :name, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.integer :postage, null: false
 
       t.timestamps
     end

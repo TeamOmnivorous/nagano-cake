8a9c3c40d6c62987c3a2a4519bf8b7e3b08bd57c
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-10-19 08:58:00 +0000

    [Add] homes#topページ

--- db/schema.rb
+++ db/schema.rb
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 2022_10_19_044007) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

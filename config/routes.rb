2a3f4c87b94f807f89d15ad995e3ff54fd91623f
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-10-20 06:31:14 +0000

    新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :customers
   devise_for :admins
- # devise_for :customers
+# devise_for :customers, :controllers => {
+#     :registrations => 'customers/registrations',
+#     :sessions => 'customers/sessions'
+#   }
+#   devise_scope :customers do
+#     get "sign_in", :to => "customers/sessions#new"
+#     get "/customers/sign_out", :to => "customers/sessions#destroy"
+#   end
 
   root to: "homes#top"
   get '/home/about' => "homes#about", as: "about"
@@ -12,6 +19,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :deliveries
   resources :items
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

6c176bbc44ded43c1a66bdaa6a42361b4dd43b21
AsheDuran <bady.0222.monk@gmail.com>
2022-10-20 09:25:56 +0000

    Item_inde_view

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,8 @@ class Item < ApplicationRecord
 
   def get_item_image(width, height)
     unless item_image.attached?
-      file_path = Rails.root.join('app/assets/images/logo4.png')
-      item_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     item_image.variant(resize_to_limit: [100, 100]).processed
   end

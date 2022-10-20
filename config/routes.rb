882da24168946eee2811b38f162e978fa8be1bc7
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-10-20 11:58:28 +0000

    管理者登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :customers
-  devise_for :admins
-
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
   root to: "homes#top"
   get '/home/about' => "homes#about", as: "about"

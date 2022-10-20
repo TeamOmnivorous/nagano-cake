8238b98c56d4fcd249846ce8cd105d07ecc301bd
AsheDuran <bady.0222.monk@gmail.com>
2022-10-20 07:17:56 +0000

    Item_inde_view

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_many :cart_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
+  has_one_attached :item_image
+
+  def get_item_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/logo4.png')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+    end
+    item_image.variant(resize_to_limit: [100, 100]).processed
+  end
 end

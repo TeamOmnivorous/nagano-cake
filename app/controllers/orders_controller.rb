9b956c3c997a44488f0f572064000dbfa29c89a3
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-10-22 08:02:00 +0000

    [Add] order関連viewページ作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,25 @@
 class OrdersController < ApplicationController
+  # before_action :permit_params, except: :new
+
+  def new
+  end
+
+  def creat
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+
+  private
+
 end

b2e06e5fce0659c4bfa075b584b65898350e9b8a
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-10-22 08:02:12 +0000

    cart_item半端者

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,18 @@
 class CartItemsController < ApplicationController
+ def index
+  	@cart_items = CartItem.all
+ end
+
+
+
+
+
+
+
+ private
+
+ def cart_item_params
+    params.require(:cart_item).permit(:item_id)
+ end
+
 end

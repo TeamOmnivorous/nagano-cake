b7aecef5894cbb9524e7589c04a05de9789e646e
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-10-23 09:58:13 +0000

    注文履歴詳細・注文一覧途中

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,15 @@
 class Admin::OrderItemsController < ApplicationController
+
+  def update
+    @order_items = OrderItem.find(params[:id])
+    @order_items.update(order_items_params)
+    redirect_to admin_orders_path(@order)
+  end
+
+  private
+
+  def order_items_params
+    params.require(:order_items).permit(:item_id,:order_id,:amount,:price,:making_status )
+  end
+
 end

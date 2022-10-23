0793cab85abbc30f6b13b8a69049c2189dc160b2
Taku Shiraishi <110590240+TakuShiraishi@users.noreply.github.com>
2022-10-23 04:07:47 +0000

    カートアイテム途中

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,8 +3,35 @@ class CartItemsController < ApplicationController
   	@cart_items = CartItem.all
  end
 
+  def create
+   @cart_item = CartItem.new(cart_item_params)
+    @item = @cart_item.item
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      redirect_to item_path(@item)
+    end
+  end
 
+  def update
+   @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to request.referer
+    end
+  end
 
+  def destroy
+   @cart_item = CartItem.find(params[:id])
+    if cart_items.destroy
+      redirect_to request.refere
+    end
+  end
+
+  def destroy_all
+   if CartItem.destroy_all
+      redirect_to request.refere
+   end
+  end
 
 
 
@@ -12,7 +39,7 @@ class CartItemsController < ApplicationController
  private
 
  def cart_item_params
-    params.require(:cart_item).permit(:item_id)
+    params.require(:cart_item).permit(:item_id,:amount)
  end
 
 end

76f5a964975447a8c719de280eea20292afe8fd3
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-10-26 14:32:15 +0000

    注文ステータス自動更新完成

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -8,6 +8,9 @@ class Admin::OrderItemsController < ApplicationController
 
 
     if @order_items.update(order_items_params)
+      if @order.order_items.count == @order.order_items.where(making_status: "製作完了").count
+        @order.update(order_status: "発送準備中")
+      end
       if @order_items.making_status == '製作中'
         @order.update(order_status: 2)
       end
